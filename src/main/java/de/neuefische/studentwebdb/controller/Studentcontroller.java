9a2c90acb28ca04b9a3627cde3304616e6383db4
Roman Kuite <roman.kuite@gmail.com>
2021-02-01 15:59:06 +0100

    Added Response Status Exceptions in case of exceeding ID range

--- src/main/java/de/neuefische/studentwebdb/controller/Studentcontroller.java
+++ src/main/java/de/neuefische/studentwebdb/controller/Studentcontroller.java
@@ -2,10 +2,12 @@ package de.neuefische.studentwebdb.controller;
 
 import de.neuefische.studentwebdb.service.StudentsService;
 import de.neuefische.studentwebdb.model.Student;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +25,9 @@ public class Studentcontroller {
 
     @GetMapping("{id}")
     public Optional<Student> getStudentById(@PathVariable int id){
-        return studentsService.getStudentById(id);
+        if(studentsService.getStudentById(id).isPresent()) {
+            return studentsService.getStudentById(id);
+        } throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Not Found!!!");
     }
 
 
--- src/main/java/de/neuefische/studentwebdb/controller/TeacherController.java
+++ src/main/java/de/neuefische/studentwebdb/controller/TeacherController.java
@@ -2,12 +2,13 @@ package de.neuefische.studentwebdb.controller;
 
 import de.neuefische.studentwebdb.model.Teacher;
 import de.neuefische.studentwebdb.service.StudentsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +25,9 @@ public class TeacherController {
 
     @GetMapping("{id}")
     public Optional<Teacher> getTeacherById(@PathVariable int id){
-        return studentsService.getTeacherById(id);
+        if(studentsService.getTeacherById(id).isPresent()) {
+            return studentsService.getTeacherById(id);
+        } throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Not Found!!!");
     }
 
 }
